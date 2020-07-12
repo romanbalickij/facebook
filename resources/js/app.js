fb72dea68e823ba5e6e185efbebc0067169534c7
roman@roman <romanbalickij9@gmail.com>
2020-07-12 16:11:34 +0300

    prepare app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
+import router from "./router";
+import App from './components/App'
+
+
+
 require('./bootstrap');
+window.Vue = require('vue');
+
+
+
+
+const app = new Vue({
+    el: '#app',
+    router,
+
+    components:{
+        App,
+    }
+
+
+});
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    fdfdfdfd
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Start"
+    }
+</script>
+
+<style scoped>
+
+</style>
